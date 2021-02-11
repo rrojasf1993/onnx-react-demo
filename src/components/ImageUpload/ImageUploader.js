95bef8a428674fda21c3b2966aab4e35f0a6ebf1
Roberto Rojas <Roberto.Rojas@teaminternational.com>
2021-02-10 19:31:00 -0500

    First version

---
+++ src/components/ImageUpload/ImageUploader.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import Dropzone from 'react-dropzone';
+import { Container, Row, Table, Col } from 'reactstrap';
+
+export const ImageUploader = (props) => 
+{
+    const [uploadedImages, setUploadedImages] = useState([]);
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <div>
+                        <h2>Cargar imagenes para analizar</h2>
+                        <Dropzone onDrop={uploadedFiles => {
+                            setUploadedImages(uploadedFiles)
+                        }}>
+                            {({ getRootProps, getInputProps }) => (
+                                <section>
+                                    <div {...getRootProps()}>
+                                        <input {...getInputProps()} />
+                                        <p>Arrastre una imagen , o haga click para seleccionar</p>
+                                    </div>
+                                </section>
+                            )}
+                        </Dropzone>
+                    </div>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Table bordered>
+                        <thead>
+                            <tr>
+                                <th>Imagenes cargadas</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                uploadedImages.map((image, index) => {
+                                    return (<tr key={index}>
+                                        <td ><Row><Col><p>{image.name}-  Tamaño: {image.size} bytes</p></Col></Row></td>
+                                    </tr>)
+                                })
+                            }
+                        </tbody>
+                    </Table>
+                </Col>
+            </Row>
+        </Container>)
+}
