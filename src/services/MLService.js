34253775fcb52bf8b742da2ecde16e677c015ad3
Roberto Rojas <Roberto.Rojas@teaminternational.com>
2021-02-25 23:44:44 -0500

    added onnx model

--- src/services/MLService.js
+++ src/services/MLService.js
@@ -1,6 +1,11 @@
 
 import {Tensor, InferenceSession} from 'onnxjs';
-const getOnnxModelInstance=()=>
+const getOnnxModelInstance= async()=>
 {
-    var mlSession=new InferenceSession({});
+    var mlSession=new InferenceSession({
+        backendHint:"cpu"
+    });
+    var model=await mlSession.loadModel('../model/Model.onnx');
+    return model;
 }
+
