e8cd51dbaa44a7b73442b71414cbaefcd5640097
Roberto Rojas <Roberto.Rojas@teaminternational.com>
2021-03-02 00:29:49 -0500

    added logic to process images

--- src/services/MLService.js
+++ src/services/MLService.js
@@ -1,11 +1,24 @@
 
+import loadImage from 'blueimp-load-image';
 import {Tensor, InferenceSession} from 'onnxjs';
 const getOnnxModelInstance= async()=>
 {
-    var mlSession=new InferenceSession({
+    let mlSession=new InferenceSession({
         backendHint:"cpu"
     });
-    var model=await mlSession.loadModel('../model/Model.onnx');
+    let model=await mlSession.loadModel('../model/Model.onnx');
     return model;
 }
 
+const loadImages=async(files)=>
+{
+    let imageLoadResults=[];
+    for(let i=0; i<=files.length; i++)
+    {
+        let file = files[i];
+        if(file){
+           let uploadResult=await loadImage(files,{maxHeight:416, maxWidth:416, canvas:true});
+           imageLoadResults.push(uploadResult);
+        }
+    }
+}
